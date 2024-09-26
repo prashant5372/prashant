3aa703074a77f77913c57e272aa4167089a2e55d
prashant5372 <prashantpranav51@gmail.com>
2024-09-26 13:10:44 +0530

    odd even

---
+++ odd even .py
@@ -0,0 +1,8 @@
+#wap to check if anumber entered by user is odd or even
+num=int(input("enter the number ="))
+  
+
+if(num%2==0):
+    print("given number is even number:")
+else:
+    print("given number is not even number :")    
